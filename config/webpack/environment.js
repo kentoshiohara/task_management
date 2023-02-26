72d8e29ae471c4e8015acdfcf417744d442dd00b
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-02-26 10:30:08 +0900

    deviseの導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,7 +7,7 @@ environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
     // $: 'jquery',
-    jQuery: 'jquery',
+    // jQuery: 'jquery',
     Popper: ['popper.js', 'default']
   })
 )
