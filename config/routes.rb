98080ee405b566a7069e0295d48fa6913b91c6ba
kentoshiohara <120357291+kentoshiohara@users.noreply.github.com>
2023-02-26 22:04:51 +0900

    cardのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations'}
   root to: 'top#index'
-  resources :lists, only: [:new, :create, :edit, :update, :destroy]
+  resources :lists, only: [:new, :create, :edit, :update, :destroy] do
+    resources :cards, only: [:new, :create]
+  end
 end
